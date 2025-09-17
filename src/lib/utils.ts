ba7f25b0d462fe3a9a3250d8c77352f440d16818
honganhh34 <honganhvo344@gmail.com>
2025-09-17 23:03:56 +0700

    18: cart an checkout

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 export function generateTenantURL(tenantSlug:string){
-   return `/tenants/${tenantSlug}`
+   return `/tenants/${tenantSlug}`;
 }
 
 export function formatCurrency(value: number | string) {
