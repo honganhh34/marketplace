495c3e5beea645eae5155767c95fee036c57874c
honganhh34 <honganhvo344@gmail.com>
2025-09-21 23:23:41 +0700

    27: subdomain rewites

--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
         <nav className=" border-t font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto h-full flex items-center gap-2 px-4 lg:px-12 py-6">
                 <p >Powered by</p>
-                <Link href='/'>
+                <Link href={process.env.NEXT_PUBLIC_APP_URL!}>
                      <span className={cn("text-2xl font-semibold",poppins.className)}>
                         funroad
                      </span>
