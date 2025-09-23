9dc140a8ba85ad52980b440ca6e5739c4d6cb680
honganhh34 <honganhvo344@gmail.com>
2025-09-23 20:29:03 +0700

    28: deployment fix 2

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 export function generateTenantURL(tenantSlug:string){
   const isDevelopment = process.env.NODE_ENV === "development";
-  const isSubdomainRoutingEnabled = Boolean(process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING!);
+  const isSubdomainRoutingEnabled = process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING  === "true";
   //in dev mode, use normal routing
-  if(process.env.NODE_ENV === "development"){
+  if(isDevelopment || !isSubdomainRoutingEnabled){
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
   const protocol = "https";
